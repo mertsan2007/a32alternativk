911a1036564ac0e403f4a83336747efd6e8358f8
Sean Young <sean@mess.org>
2017-09-01 11:34:23 -0300

    UPSTREAM: [media] media: rc: include device name in rc udev event
    This name is also stored in the input's device name, but that
    is not available in TX only hardware (no input device).
    
    Signed-off-by: Sean Young <sean@mess.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@osg.samsung.com>

--- drivers/media/rc/rc-main.c
+++ drivers/media/rc/rc-main.c
@@ -1573,7 +1573,12 @@ static int rc_dev_uevent(struct device *device, struct kobj_uevent_env *env)
 	struct rc_dev *dev = to_rc_dev(device);
 	int ret = 0;
 
-	mutex_lock(&dev->lock);
+	if (dev->rc_map.name)
+		ADD_HOTPLUG_VAR("NAME=%s", dev->rc_map.name);
+	if (dev->driver_name)
+		ADD_HOTPLUG_VAR("DRV_NAME=%s", dev->driver_name);
+	if (dev->device_name)
+		ADD_HOTPLUG_VAR("DEV_NAME=%s", dev->device_name);
 
 	if (!dev->registered)
 		ret = -ENODEV;
