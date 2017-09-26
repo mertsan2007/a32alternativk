2c981cdbae28963c58eca2134ee51c3af0d1b26b
Sean Young <sean@mess.org>
2017-09-26 07:31:29 -0400

    UPSTREAM: media: lirc: move lirc_dev->attached to rc_dev->registered
    This is done to further remove the lirc kernel api. Ensure that every
    fops checks for this.
    
    Signed-off-by: Sean Young <sean@mess.org>
    Signed-off-by: Mauro Carvalho Chehab <mchehab@s-opensource.com>

--- drivers/media/rc/rc-main.c
+++ drivers/media/rc/rc-main.c
@@ -1911,6 +1911,8 @@ int rc_register_device(struct rc_dev *dev)
 			goto out_lirc;
 	}
 
+	dev->registered = true;
+
 	IR_dprintk(1, "Registered rc%u (driver: %s)\n",
 		   dev->minor,
 		   dev->driver_name ? dev->driver_name : "unknown");
@@ -1983,6 +1985,14 @@ void rc_unregister_device(struct rc_dev *dev)
 
 	rc_free_rx_device(dev);
 
+	mutex_lock(&dev->lock);
+	dev->registered = false;
+	mutex_unlock(&dev->lock);
+
+	/*
+	 * lirc device should be freed with dev->registered = false, so
+	 * that userspace polling will get notified.
+	 */
 	if (dev->driver_type != RC_DRIVER_SCANCODE)
 		ir_lirc_unregister(dev);
 
