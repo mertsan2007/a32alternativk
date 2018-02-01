4d0d3b4b9ac2cde6ab69824de07a2a508eeb8a03
Matthew Wilcox <mawilcox@microsoft.com>
2018-01-31 16:18:51 -0800

    UPSTREAM: mm: remove misleading alignment claims
    The "third double word block" isn't on 32-bit systems.  The layout looks
    like this:
    
    	unsigned long flags;
    	struct address_space *mapping
    	pgoff_t index;
    	atomic_t _mapcount;
    	atomic_t _refcount;
    
    which is 32 bytes on 64-bit, but 20 bytes on 32-bit.  Nobody is trying to
    use the fact that it's double-word aligned today, so just remove the
    misleading claims.
    
    Link: http://lkml.kernel.org/r/20171220155552.15884-4-willy@infradead.org
    Signed-off-by: Matthew Wilcox <mawilcox@microsoft.com>
    Acked-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Acked-by: Christoph Lameter <cl@linux.com>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Randy Dunlap <rdunlap@infradead.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>

--- include/linux/mm_types.h
+++ include/linux/mm_types.h
@@ -35,26 +35,6 @@ struct hmm;
  * a page, though if it is a pagecache page, rmap structures can tell us
  * who is mapping it.
  *
- * If you allocate the page using alloc_pages(), you can use some of the
- * space in struct page for your own purposes.  The five words in the main
- * union are available, except for bit 0 of the first word which must be
- * kept clear.  Many users use this word to store a pointer to an object
- * which is guaranteed to be aligned.  If you use the same storage as
- * page->mapping, you must restore it to NULL before freeing the page.
- *
- * If your page will not be mapped to userspace, you can also use the four
- * bytes in the mapcount union, but you must call page_mapcount_reset()
- * before freeing it.
- *
- * If you want to use the refcount field, it must be used in such a way
- * that other CPUs temporarily incrementing and then decrementing the
- * refcount does not cause problems.  On receiving the page from
- * alloc_pages(), the refcount will be positive.
- *
- * If you allocate pages of order > 0, you can use some of the fields
- * in each subpage, but you may need to restore some of their values
- * afterwards.
- *
  * SLUB uses cmpxchg_double() to atomically update its freelist and
  * counters.  That requires that freelist & counters be adjacent and
  * double-word aligned.  We align all struct pages to double-word
@@ -137,8 +117,41 @@ struct page {
 			 */
 			atomic_t _refcount;
 		};
-		struct {	/* Tail pages of compound page */
-			unsigned long compound_head;	/* Bit zero is set */
+	};
+
+	/*
+	 * WARNING: bit 0 of the first word encode PageTail(). That means
+	 * the rest users of the storage space MUST NOT use the bit to
+	 * avoid collision and false-positive PageTail().
+	 */
+	union {
+		struct list_head lru;	/* Pageout list, eg. active_list
+					 * protected by zone_lru_lock !
+					 * Can be used as a generic list
+					 * by the page owner.
+					 */
+		struct dev_pagemap *pgmap; /* ZONE_DEVICE pages are never on an
+					    * lru or handled by a slab
+					    * allocator, this points to the
+					    * hosting device page map.
+					    */
+		struct {		/* slub per cpu partial pages */
+			struct page *next;	/* Next partial slab */
+#ifdef CONFIG_64BIT
+			int pages;	/* Nr of partial slabs left */
+			int pobjects;	/* Approximate # of objects */
+#else
+			short int pages;
+			short int pobjects;
+#endif
+		};
+
+		struct rcu_head rcu_head;	/* Used by SLAB
+						 * when destroying via RCU
+						 */
+		/* Tail pages of compound page */
+		struct {
+			unsigned long compound_head; /* If bit zero is set */
 
 			/* First tail page only */
 			unsigned char compound_dtor;
@@ -172,23 +185,22 @@ struct page {
 		struct rcu_head rcu_head;
 	};
 
-	union {		/* This union is 4 bytes in size. */
-		/*
-		 * If the page can be mapped to userspace, encodes the number
-		 * of times this page is referenced by a page table.
-		 */
-		atomic_t _mapcount;
-
-		/*
-		 * If the page is neither PageSlab nor mappable to userspace,
-		 * the value stored here may help determine what this page
-		 * is used for.  See page-flags.h for a list of page types
-		 * which are currently stored here.
-		 */
-		unsigned int page_type;
-
-		unsigned int active;		/* SLAB */
-		int units;			/* SLOB */
+	union {
+		unsigned long private;		/* Mapping-private opaque data:
+					 	 * usually used for buffer_heads
+						 * if PagePrivate set; used for
+						 * swp_entry_t if PageSwapCache;
+						 * indicates order in the buddy
+						 * system if PG_buddy is set.
+						 */
+#if USE_SPLIT_PTE_PTLOCKS
+#if ALLOC_SPLIT_PTLOCKS
+		spinlock_t *ptl;
+#else
+		spinlock_t ptl;
+#endif
+#endif
+		struct kmem_cache *slab_cache;	/* SL[AU]B: Pointer to slab */
 	};
 
 	/* Usage count. *DO NOT USE DIRECTLY*. See page_ref.h */
