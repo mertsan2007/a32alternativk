842c246904f1361672ff66e8880bcac109582a03
Yonghong Song <yhs@fb.com>
2018-09-06 17:26:04 -0700

    UPSTREAM: bpf: add bpffs pretty print for program array map
    Added bpffs pretty print for program array map. For a particular
    array index, if the program array points to a valid program,
    the "<index>: <prog_id>" will be printed out like
       0: 6
    which means bpf program with id "6" is installed at index "0".
    
    Change-Id: Ibfeac1777df6dc8742debe574ba259d212e7ecea
    Signed-off-by: Yonghong Song <yhs@fb.com>
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>

--- kernel/bpf/arraymap.c
+++ kernel/bpf/arraymap.c
@@ -580,7 +580,7 @@ const struct bpf_map_ops prog_array_map_ops = {
 	.map_fd_put_ptr = prog_fd_array_put_ptr,
 	.map_fd_sys_lookup_elem = prog_fd_array_sys_lookup_elem,
 	.map_release_uref = bpf_fd_array_map_clear,
-	.map_check_btf = map_check_no_btf,
+	.map_seq_show_elem = prog_array_map_seq_show_elem,
 };
 
 static struct bpf_event_entry *bpf_event_entry_gen(struct file *perf_file,
