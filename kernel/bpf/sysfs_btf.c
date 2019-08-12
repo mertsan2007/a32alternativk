f13ace3bcc395c7facb3477072dc5d3f2fc287d0
Andrii Nakryiko <andriin@fb.com>
2019-08-12 11:39:47 -0700

    BACKPORT: btf: expose BTF info through sysfs
    Make .BTF section allocated and expose its contents through sysfs.
    
    /sys/kernel/btf directory is created to contain all the BTFs present
    inside kernel. Currently there is only kernel's main BTF, represented as
    /sys/kernel/btf/kernel file. Once kernel modules' BTFs are supported,
    each module will expose its BTF as /sys/kernel/btf/<module-name> file.
    
    Current approach relies on a few pieces coming together:
    1. pahole is used to take almost final vmlinux image (modulo .BTF and
       kallsyms) and generate .BTF section by converting DWARF info into
       BTF. This section is not allocated and not mapped to any segment,
       though, so is not yet accessible from inside kernel at runtime.
    2. objcopy dumps .BTF contents into binary file and subsequently
       convert binary file into linkable object file with automatically
       generated symbols _binary__btf_kernel_bin_start and
       _binary__btf_kernel_bin_end, pointing to start and end, respectively,
       of BTF raw data.
    3. final vmlinux image is generated by linking this object file (and
       kallsyms, if necessary). sysfs_btf.c then creates
       /sys/kernel/btf/kernel file and exposes embedded BTF contents through
       it. This allows, e.g., libbpf and bpftool access BTF info at
       well-known location, without resorting to searching for vmlinux image
       on disk (location of which is not standardized and vmlinux image
       might not be even available in some scenarios, e.g., inside qemu
       during testing).
    
    Alternative approach using .incbin assembler directive to embed BTF
    contents directly was attempted but didn't work, because sysfs_proc.o is
    not re-compiled during link-vmlinux.sh stage. This is required, though,
    to update embedded BTF data (initially empty data is embedded, then
    pahole generates BTF info and we need to regenerate sysfs_btf.o with
    updated contents, but it's too late at that point).
    
    If BTF couldn't be generated due to missing or too old pahole,
    sysfs_btf.c handles that gracefully by detecting that
    _binary__btf_kernel_bin_start (weak symbol) is 0 and not creating
    /sys/kernel/btf at all.
    
    v2->v3:
    - added Documentation/ABI/testing/sysfs-kernel-btf (Greg K-H);
    - created proper kobject (btf_kobj) for btf directory (Greg K-H);
    - undo v2 change of reusing vmlinux, as it causes extra kallsyms pass
      due to initially missing  __binary__btf_kernel_bin_{start/end} symbols;
    
    v1->v2:
    - allow kallsyms stage to re-use vmlinux generated by gen_btf();
    
    Reviewed-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Change-Id: Ife57356dd14b9ae0d2e3801eea7c602c6245952f
    Signed-off-by: Andrii Nakryiko <andriin@fb.com>
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- kernel/bpf/sysfs_btf.c
+++ kernel/bpf/sysfs_btf.c
@@ -9,37 +9,43 @@
 #include <linux/sysfs.h>
 
 /* See scripts/link-vmlinux.sh, gen_btf() func for details */
-extern char __weak __start_BTF[];
-extern char __weak __stop_BTF[];
+extern char __weak _binary__btf_kernel_bin_start[];
+extern char __weak _binary__btf_kernel_bin_end[];
 
 static ssize_t
-btf_vmlinux_read(struct file *file, struct kobject *kobj,
-		 struct bin_attribute *bin_attr,
-		 char *buf, loff_t off, size_t len)
+btf_kernel_read(struct file *file, struct kobject *kobj,
+		struct bin_attribute *bin_attr,
+		char *buf, loff_t off, size_t len)
 {
-	memcpy(buf, __start_BTF + off, len);
+	memcpy(buf, _binary__btf_kernel_bin_start + off, len);
 	return len;
 }
 
-static struct bin_attribute bin_attr_btf_vmlinux __ro_after_init = {
-	.attr = { .name = "vmlinux", .mode = 0444, },
-	.read = btf_vmlinux_read,
+static struct bin_attribute bin_attr_btf_kernel __ro_after_init = {
+	.attr = { .name = "kernel", .mode = 0444, },
+	.read = btf_kernel_read,
 };
 
 static struct kobject *btf_kobj;
 
-static int __init btf_vmlinux_init(void)
+static int __init btf_kernel_init(void)
 {
-	bin_attr_btf_vmlinux.size = __stop_BTF - __start_BTF;
+	int err;
 
-	if (!__start_BTF || bin_attr_btf_vmlinux.size == 0)
+	if (!_binary__btf_kernel_bin_start)
 		return 0;
 
 	btf_kobj = kobject_create_and_add("btf", kernel_kobj);
-	if (!btf_kobj)
-		return -ENOMEM;
+	if (IS_ERR(btf_kobj)) {
+		err = PTR_ERR(btf_kobj);
+		btf_kobj = NULL;
+		return err;
+	}
 
-	return sysfs_create_bin_file(btf_kobj, &bin_attr_btf_vmlinux);
+	bin_attr_btf_kernel.size = _binary__btf_kernel_bin_end -
+				   _binary__btf_kernel_bin_start;
+
+	return sysfs_create_bin_file(btf_kobj, &bin_attr_btf_kernel);
 }
 
-subsys_initcall(btf_vmlinux_init);
+subsys_initcall(btf_kernel_init);
